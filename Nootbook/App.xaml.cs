9bf79bdaf3ba3dd82b7b4623717fa2d24271c129
HUBERT <u4546006479@gmail.com>
2026-09-09 08:22:34 +0200

    Ajoutez des fichiers projet.

---
+++ Nootbook/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Nootbook
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
