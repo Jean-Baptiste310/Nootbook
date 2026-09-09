2003595c39b35b9a478b10b39c933589221f7a75
HUBERT <u4546006479@gmail.com>
2026-09-09 09:25:43 +0200

    Test unitaire

---
+++ TestLogic/TestEducatonalElement.cs
@@ -0,0 +1,21 @@
+﻿using Logic;
+using System.Reflection;
+
+namespace TestLogic
+{
+    public class TestEducatonalElement
+    {
+        [Fact]
+        public void Testexeption()
+        {
+            EducationalElement element = new EducationalElement();
+            Assert.Throws<Exception>(() => { element.Name = ""; });   
+        }
+        [Fact]
+        public void TestCoefException()
+        {
+            EducationalElement element = new EducationalElement();
+            Assert.Throws<Exception>(() => { element.Coef = 0; });
+        }
+    }
+}
