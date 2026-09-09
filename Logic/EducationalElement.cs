b42b65cd0dc3b071d8938a31645f51e605be72cc
HUBERT <u4546006479@gmail.com>
2026-09-09 08:54:46 +0200

    Premier commit class

---
+++ Logic/EducationalElement.cs
@@ -0,0 +1,32 @@
+﻿namespace Logic
+{
+    public class EducationalElement
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("The name cannot be null or empty");
+                }
+                name = value; 
+            }
+        }
+        private float coef;
+        public float Coef
+        {
+            get { return  coef; }
+            set {  
+                if (value <= 0)
+                {
+                    throw new Exception("The coef must be >0");
+                }
+                coef = value; 
+            }
+        }
+
+    }
+}
