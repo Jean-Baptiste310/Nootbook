0e467efea6fb18672da357127379c26fadda8b92
HUBERT <u4546006479@gmail.com>
2026-09-24 08:51:45 +0200

    Partie 3

--- Logic/EducationalElement.cs
+++ Logic/EducationalElement.cs
@@ -28,5 +28,10 @@
             }
         }
 
+        public override string ToString()
+        {
+            return name + " (" + coef.ToString() + ")";
+        }
+
     }
 }
--- TestLogic/TestEducatonalElement.cs
+++ TestLogic/TestEducatonalElement.cs
@@ -9,13 +9,17 @@ namespace TestLogic
         public void Testexeption()
         {
             EducationalElement element = new EducationalElement();
-            Assert.Throws<Exception>(() => { element.Name = ""; });   
+            Assert.Throws<Exception>(() => { element.Name = ""; });
+            element.Name = "math";
+            Assert.Equal("math", element.Name);
         }
         [Fact]
         public void TestCoefException()
         {
             EducationalElement element = new EducationalElement();
             Assert.Throws<Exception>(() => { element.Coef = 0; });
+            element.Coef = 1.5f;
+            Assert.Equal(1.5, element.Coef, 3);
         }
     }
 }
